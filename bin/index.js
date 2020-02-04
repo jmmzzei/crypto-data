bc7af4c57fbf5ffa32b954dd2aee978e4e01719f
jmm <josemmazzei@gmail.com>
2020-02-04 20:15:08 -0300

    Inicial commit with basic functionality

---
+++ bin/index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+const chalk = require('chalk')
+const fetch = require('node-fetch')
+var inquirer = require('inquirer');
+const program = require('commander')
+const dotenv = require('dotenv')
+dotenv.config()
+
+program
+    .arguments('get <crypto>', 'get crypto value')
+    .option('-o, --option', 'Add option')
+    .option('-c, --coin <type>', 'Add a coin')
+    .version('0.0.1')
+
+program.parse(process.argv)
+
+console.log(chalk.green.inverse(' CRYPTO DATA FETCHER '))
+
+if (program.coin) {
+    fetchCoins(program.coin)
+} else {
+    askAndFetch()
+}
+
+function askAndFetch() {
+    inquirer.prompt({
+        type: 'input',
+        name: 'coin',
+        message: 'Insert a coin:'
+    }).then(answers => {
+        
+        if (answers.coin.trim() !== "") {
+            fetchCoins(answers.coin)
+        } else {
+            console.log(chalk.red('You must specify a coin or press CTRL + C to exit.'))
+            askAndFetch()
+        }
+    })
+}
+
+function fetchCoins(coin) {
+    console.log(`Fetching for: ${coin}...`)
+
+    fetch('https://rest.coinapi.io/v1/assets/', {
+        "X-CoinAPI-Key": process.env.COIN_API
+    }).then(res => res.json())
+        .then(res => {
+            let filtered = res.filter(e => e.name === coin)
+
+            if (filtered.length === 0) {
+                console.log(`No data about ${coin}`);
+            } else {
+                console.log(
+                    `${chalk.magentaBright.inverse('[ Results ]')}    
+                
+                ${chalk.blue.inverse(`Coin: ${filtered[0].name} (${filtered[0].asset_id})`)}
+                
+                ${chalk.red.inverse(`Price: $${filtered[0].price_usd}`)}
+                
+                ${chalk.white.inverse(`Volume (1hr): ${filtered[0].volume_1hrs_usd}`)}
+                ${chalk.white.inverse(`Volume (1day): ${filtered[0].volume_1day_usd}`)}
+                ${chalk.white.inverse(`Volume (1mth): ${filtered[0].volume_1mth_usd}`)}
+                ${chalk.white.inverse(`Volume (1hr): ${filtered[0].volume_1hrs_usd}`)}
+                `)
+            }
+        })
+}
