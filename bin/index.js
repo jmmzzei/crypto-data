7c23e3196d29a8d1c32ed660dc4bf31bc9bc6a28
jmm <josemmazzei@gmail.com>
2020-02-05 21:13:31 -0300

    Add cfonts title

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const chalk = require('chalk')
 const fetch = require('node-fetch')
-var inquirer = require('inquirer');
+const inquirer = require('inquirer');
 const program = require('commander')
 const dotenv = require('dotenv')
+const cfont = require('cfonts') 
 dotenv.config()
 
 program
@@ -14,7 +15,10 @@ program
 
 program.parse(process.argv)
 
-console.log(chalk.green.inverse(' CRYPTO DATA FETCHER '))
+cfont.say('CRYPTO DATA', {
+    font: 'block',
+    align: 'center'
+})
 
 if (program.coin) {
     fetchCoins(program.coin)
@@ -57,10 +61,10 @@ function fetchCoins(coin) {
                 
                 ${chalk.red.inverse(`Price: $${filtered[0].price_usd}`)}
                 
-                ${chalk.white.inverse(`Volume (1hr): ${filtered[0].volume_1hrs_usd}`)}
-                ${chalk.white.inverse(`Volume (1day): ${filtered[0].volume_1day_usd}`)}
-                ${chalk.white.inverse(`Volume (1mth): ${filtered[0].volume_1mth_usd}`)}
-                ${chalk.white.inverse(`Volume (1hr): ${filtered[0].volume_1hrs_usd}`)}
+                ${chalk.white.inverse(`Volume (1hr): $${filtered[0].volume_1hrs_usd}`)}
+                ${chalk.white.inverse(`Volume (1day): $${filtered[0].volume_1day_usd}`)}
+                ${chalk.white.inverse(`Volume (1mth): $${filtered[0].volume_1mth_usd}`)}
+                ${chalk.white.inverse(`Volume (1hr): $${filtered[0].volume_1hrs_usd}`)}
                 `)
             }
         })
