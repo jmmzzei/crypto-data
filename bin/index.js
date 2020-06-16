0aac429e8ae39f231a002874e719331cbb737c88
jmm <josemmazzei@gmail.com>
2020-06-16 09:12:46 -0300

    Add conditional rendering based on terminal color support
    supports-color package was added for identify if the user's terminal
    supports 16m colors.

--- bin/index.js
+++ bin/index.js
@@ -10,22 +10,22 @@ const printTitle = require('../lib/printTitle')
 dotenv.config()
 
 program
-	.arguments("get <crypto>", "get crypto value")
-	.option("-c, --coin <coin>", "Fetch data for a specific coin")
-	.option("-f, --add-fav <coin>", "Add a coin to favorites")
-	.option("-r, --rm-fav <coin>", "Remove a coin from favorites")
-	.version("1.0.0")
+    .arguments("get <crypto>", "get crypto value")
+    .option("-c, --coin <coin>", "Fetch data for a specific coin")
+    .option("-f, --add-fav <coin>", "Add a coin to favorites")
+    .option("-r, --rm-fav <coin>", "Remove a coin from favorites")
+    .version("1.0.7")
 
 program.parse(process.argv)
 
 if (program.coin) {
-	printTitle()
-	fetchAndDisplay(program.coin)
+    printTitle()
+    fetchAndDisplay(program.coin)
 } else if (program.addFav) {
-	addToFav(program.addFav)
+    addToFav(program.addFav)
 } else if (program.rmFav) {
-	removeFromFav(program.rmFav)
+    removeFromFav(program.rmFav)
 } else {
-	printTitle()
-	selectFavsOrCurstomSearch()
+    printTitle()
+    selectFavsOrCurstomSearch()
 }
